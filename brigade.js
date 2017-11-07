0bc6a5e2fa6b470f1f6b7d22116699e4868189d5
Brian Redmond <brianisrunning@gmail.com>
2017-11-07 09:36:40 -0500

    added steps to brigade.js

---
+++ brigade.js
@@ -0,0 +1,24 @@
+const { events, Job, Group } = require('brigadier')
+
+events.on("push", function(e, project) {
+    console.log("received push for commit " + e.commit)
+  
+    // Create a new job
+    var node = new Job("job-runner-golang")
+    node.storage.enabled = false
+  
+    // use golang image for first step
+    node.image = "golang:1.7.5"
+  
+    // Now we want it to run these commands in order:
+    node.tasks = [
+      "cd /src/",
+      "ls -la",
+      "go get github.com/gorilla/mux",
+      "cd smackapi && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o smackapi",
+      "go test -v"
+    ]
+  
+    // We're done configuring, so we run the job
+    node.run()
+  })
