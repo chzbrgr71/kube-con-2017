0f146be0c0d2993712502507f1678454a1dda912
Brian Redmond <brianisrunning@gmail.com>
2017-11-08 11:28:47 -0500

    updates

--- brigade.js
+++ brigade.js
@@ -1,7 +1,7 @@
 const { events, Job, Group } = require('brigadier')
 
 events.on("push", function(e, project) {
-    console.log("received push for commit " + e.commit)
+    console.log("==> received push for commit " + e.commit)
   
     // create a new job for go work
     var golang = new Job("job-runner-golang")
