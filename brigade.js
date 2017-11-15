767f1696de3adf7cdbce353dbc2c203ebd0e9730
Brian Redmond <brianisrunning@gmail.com>
2017-11-15 09:17:15 -0700

    updated without PR

--- brigade.js
+++ brigade.js
@@ -1,6 +1,6 @@
 const { events, Job, Group } = require('brigadier')
 
-events.on("push", function(e, project) {
+events.on("pull_request", function(e, project) {
     console.log("==> received push for commit " + e.commit)
 
     // setup variables
