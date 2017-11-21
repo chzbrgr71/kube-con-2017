7ce26eb79873b7e120882acc84bb1eb3004b33c0
Brian Redmond <brianisrunning@gmail.com>
2017-11-21 11:44:02 -0500

    updated

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ const { events, Job, Group } = require('brigadier')
 
 events.on("push", (brigadeEvent, project) => {
     
-    // setup variables in map
+    // setup variables
     var gitPayload = JSON.parse(brigadeEvent.payload)
     var brigConfig = new Map()
     brigConfig.set("acrServer", project.secrets.acrServer)
@@ -23,7 +23,7 @@ events.on("push", (brigadeEvent, project) => {
     var helm = new Job("job-runner-helm")
     goJobRunner(golang)
     dockerJobRunner(brigConfig, docker)
-    helmJobRunner(brigConfig, helm, 100, 0)
+    helmJobRunner(brigConfig, helm, 100, 0, "prod")
 
     // start pipeline
     console.log(`==> starting pipeline for docker image: ${brigConfig.get("apiACRImage")}:${brigConfig.get("imageTag")}`)
@@ -39,70 +39,37 @@ events.on("push", (brigadeEvent, project) => {
 })
 
 events.on("pull_request", (e, project) => {
-    console.log("==> received pull request for commit " + e.commit)
 
     // setup variables
-    var acrServer = project.secrets.acrServer
-    var acrUsername = project.secrets.acrUsername
-    var acrPassword = project.secrets.acrPassword
-    var apiImage = "chzbrgr71/smackapi"
-    var gitSHA = e.commit.substr(0,7)
-    var eventType = e.type
-    if (eventType === "push") {
-        var imageTag = `prod-${gitSHA}`
-    } else {
-        var imageTag = `${eventType}-${gitSHA}`
-    }
-    var apiACRImage = `${acrServer}/${apiImage}`
-    console.log(`==> docker image for ACR is ${apiACRImage}:${imageTag}`)
+    var gitPayload = JSON.parse(brigadeEvent.payload)
+    var brigConfig = new Map()
+    brigConfig.set("acrServer", project.secrets.acrServer)
+    brigConfig.set("acrUsername", project.secrets.acrUsername)
+    brigConfig.set("acrPassword", project.secrets.acrPassword)
+    brigConfig.set("apiImage", "chzbrgr71/smackapi")
+    brigConfig.set("gitSHA", brigadeEvent.commit.substr(0,7))
+    brigConfig.set("eventType", brigadeEvent.type)
+    brigConfig.set("branch", getBranch(gitPayload))
+    brigConfig.set("imageTag", `${brigConfig.get("branch")}-${brigConfig.get("gitSHA")}`)
+    brigConfig.set("apiACRImage", `${brigConfig.get("acrServer")}/${brigConfig.get("apiImage")}`)
+    
+    console.log(`==> gitHub webook (${brigConfig.get("branch")}) with commit ID ${brigConfig.get("gitSHA")}`)
 
-    // define job for golang work
+    // setup brigade jobs
     var golang = new Job("job-runner-golang")
-    golang.storage.enabled = false
-    golang.image = "golang:1.7.5"
-    golang.tasks = [
-        "cd /src/",
-        "go get github.com/gorilla/mux",
-        "cd smackapi && GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o smackapi",
-        "go test -v"
-    ]
-    
-    // define job for docker work
     var docker = new Job("job-runner-docker")
-    docker.storage.enabled = false
-    docker.image = "chzbrgr71/dnd:v5"
-    docker.privileged = true
-    docker.tasks = [
-        "dockerd-entrypoint.sh &",
-        "echo waiting && sleep 20",
-        "cd /src/smackapi/",
-        `docker login ${acrServer} -u ${acrUsername} -p ${acrPassword}`,
-        "go get github.com/gorilla/mux",
-        "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o smackapi",
-        `docker build --build-arg BUILD_DATE='1/1/2017 5:00' --build-arg IMAGE_TAG_REF=${imageTag} --build-arg VCS_REF=${gitSHA} -t ${apiImage} .`,
-        `docker tag ${apiImage} ${apiACRImage}:${imageTag}`,
-        `docker push ${apiACRImage}:${imageTag}`,
-        "killall dockerd"
-    ]
-    
-    // define job for k8s/helm work
     var helm = new Job("job-runner-helm")
-    helm.storage.enabled = false
-    helm.image = "lachlanevenson/k8s-helm:2.7.0"
-    helm.tasks = [
-        "cd /src/",
-        "helm version",
-        `helm upgrade --install smackapi-new ./charts/smackapi --namespace microsmack --set api.image=${apiACRImage} --set api.imageTag=${imageTag} --set api.deployment=smackapi-new --set api.versionLabel=new`,
-        `helm upgrade --install microsmack-routes ./charts/routes --namespace microsmack --set prodLabel=prod --set prodWeight=10 --set canaryLabel=new --set canaryWeight=90`
-    ]
+    goJobRunner(golang)
+    dockerJobRunner(brigConfig, docker)
+    helmJobRunner(brigConfig, helm, 100, 0, "new")
 
-    console.log("==> starting pipeline steps")
+    // start pipeline
+    console.log(`==> starting pipeline for docker image: ${brigConfig.get("apiACRImage")}:${brigConfig.get("imageTag")}`)
     var pipeline = new Group()
     pipeline.add(golang)
     pipeline.add(docker)
     pipeline.add(helm)
     pipeline.runEach()
-
 })
 
 function getBranch (p) {
@@ -143,12 +110,12 @@ function dockerJobRunner(config, d) {
     ]
 }
 
-function helmJobRunner (config, h, prodWeight, canaryWeight) {
+function helmJobRunner (config, h, prodWeight, canaryWeight, deployType) {
     h.storage.enabled = false
     h.image = "lachlanevenson/k8s-helm:2.7.0"
     h.tasks = [
         "cd /src/",
-        `helm upgrade --install smackapi-prod ./charts/smackapi --namespace microsmack --set api.image=${config.get("apiACRImage")} --set api.imageTag=${config.get("imageTag")} --set api.deployment=smackapi-prod --set api.versionLabel=prod`,
+        `helm upgrade --install smackapi-${deployType} ./charts/smackapi --namespace microsmack --set api.image=${config.get("apiACRImage")} --set api.imageTag=${config.get("imageTag")} --set api.deployment=smackapi-${deployType} --set api.versionLabel=${deployType}`,
         `helm upgrade --install microsmack-routes ./charts/routes --namespace microsmack --set prodLabel=prod --set prodWeight=${prodWeight} --set canaryLabel=new --set canaryWeight=${canaryWeight}`
     ]
 }
